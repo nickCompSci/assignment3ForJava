c962f82011fa67eef780fc8f9cb18e49c14a1652
Nicholas Shapovalov <120406902@umail.ucc.ie>
2022-12-01 13:31:11 +0000

    Feat: Add checks in Colour class for illegal range for floating numbers to satisfy new test
    Signed-off-by: Nicholas Shapovalov <120406902@umail.ucc.ie>

--- src/main/java/org/nicholas/Colour.java
+++ src/main/java/org/nicholas/Colour.java
@@ -2,5 +2,14 @@ package org.nicholas;
 
 public class Colour {
     public Colour(float red, float green, float blue) {
+        if (red > 1.0 || red < 0.0){
+            throw new IllegalArgumentException("Values for red, green and blue must be in range 0.0 and 1.0");
+        }
+        if (green > 1.0 || green < 0.0){
+            throw new IllegalArgumentException("Values for red, green and blue must be in range 0.0 and 1.0");
+        }
+        if (blue > 1.0 || blue < 0.0){
+            throw new IllegalArgumentException("Values for red, green and blue must be in range 0.0 and 1.0");
+        }
     }
 }
