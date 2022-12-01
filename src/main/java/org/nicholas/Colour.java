f4488845cef70417ad612b023e3876234c328f9b
Nicholas Shapovalov <120406902@umail.ucc.ie>
2022-12-01 20:39:58 +0000

    Feat: Add private float variable 'blue' which is assigned in constructor and has a getter
    Signed-off-by: Nicholas Shapovalov <120406902@umail.ucc.ie>

--- src/main/java/org/nicholas/Colour.java
+++ src/main/java/org/nicholas/Colour.java
@@ -3,6 +3,7 @@ package org.nicholas;
 public class Colour {
     private float red;
     private float green;
+    private float blue;
     public Colour(float red, float green, float blue) {
         if (red > 1.0 || red < 0.0){
             throw new IllegalArgumentException("Values for red, green and blue must be in range 0.0 and 1.0");
@@ -15,6 +16,7 @@ public class Colour {
         }
         this.red = red;
         this.green = green;
+        this.blue = blue;
     }
     public Colour(int combinedRgbValue){
 
@@ -26,5 +28,9 @@ public class Colour {
 
     public float getGreenValue() {
         return this.green;
+    }
+
+    public float getBlueValue() {
+        return this.blue;
     }
 }
