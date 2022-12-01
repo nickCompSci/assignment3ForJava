f954a8a7d40ea2ca94253d9dfce17292673abd3d
Nicholas Shapovalov <120406902@umail.ucc.ie>
2022-12-01 20:22:13 +0000

    Test: Create new test method for private value of Red from Floating constructor
    Signed-off-by: Nicholas Shapovalov <120406902@umail.ucc.ie>

--- src/test/java/org/nicholas/ColourTest.java
+++ src/test/java/org/nicholas/ColourTest.java
@@ -14,6 +14,7 @@ public class ColourTest {
         Colour testColourConstructor2 = new Colour(500);
         assertNotNull(testColourConstructor2, "This Colour object is null!");
     }
+
     @Test
     void testColourConstructorRange(){
         Exception exception = assertThrows(
@@ -24,4 +25,11 @@ public class ColourTest {
         );
         assertEquals("Values for red, green and blue must be in range 0.0 and 1.0", exception.getMessage());
     }
+
+    @Test
+    void testFloatConstructorGetterForRed(){
+        Colour testRedValueOfColour = new Colour(0.2f, 0.5f, 0.3f);
+        assertEquals(0.2f, testRedValueOfColour.getRedValue());
+    }
+
 }
